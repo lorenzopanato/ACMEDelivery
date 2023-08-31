c2fedad81f5cc1e548457fcfead546e117543dfa
Lorenzo Panato <lorenzohpanato@gmail.com>
2023-08-30 21:27:01 -0300

    criando entrada e saida de arquivos

--- src/app/ACMEDelivery.java
+++ src/app/ACMEDelivery.java
@@ -1,8 +1,27 @@
 package app;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class ACMEDelivery {
+
+    private Scanner entrada;
+    private BufferedWriter saida;
+
+    public ACMEDelivery() {
+        try {
+            BufferedReader streamEntrada = new BufferedReader(new FileReader("arquivoentrada.txt"));
+            entrada = new Scanner(streamEntrada);
+            saida = new BufferedWriter(new FileWriter("arquivosaida.txt"));
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 
     public void executar() {
 
     }
+
+
 }
